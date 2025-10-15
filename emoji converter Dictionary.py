3bbf36a4e3576e9edbb2e8eeccc4acbd51ee60f5
Abdul Haqq <yussifabdulhaqq@gmail.com>
2025-10-15 21:14:45 +0000

    Add initial project files and implement basic functionality for various scripts

---
+++ emoji converter Dictionary.py
@@ -0,0 +1,11 @@
+message = input(">")
+words = message.split(' ')
+emojis = {
+    ":)" : "😃",
+    ":(" : "😔"
+}
+output = ""
+for word in words:
+    emojis.get(word, word)
+    output += emojis.get(word, word) + " "
+print(output)
