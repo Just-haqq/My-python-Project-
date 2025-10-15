9873cf31d44d026ac0bc1a5b161f62f63f96567d
Abdul Haqq <yussifabdulhaqq@gmail.com>
2025-10-15 21:17:27 +0000

    This is my new python file showinh my journey

---
+++ Classes.py
@@ -0,0 +1,12 @@
+class Point:
+    def move(self):
+        print("move")
+
+
+    def draw(self):
+        print("draw")
+
+
+point1 = Point()
+point1.draw()
+print(point1)
--- Functions and parameters.py
+++ Functions and parameters.py
@@ -4,5 +4,5 @@ def greet_user(first_name, last_name):
 
 
 print("Start")
-greet_user("John", "smith")
+greet_user(last_name="Smith", first_name="John")
 print("Finish")
