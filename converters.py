a45902b4305f5e65963c61d850fc305d57386601
Abdul Haqq <yussifabdulhaqq@gmail.com>
2025-12-09 15:14:02 +0000

    Refactor converters.py: clean up unused find_max function and adjust formatting

--- converters.py
+++ converters.py
@@ -1,14 +1,6 @@
 def lbs_to_kg(weight):
     return weight * 0.45
 
-
+    
 def kg_to_lbs(weight):
-    return weight / 0.45
-
-
-def find_max(numbers):
-    maximum = numbers[0]
-    for number in numbers:
-        if number > maximum:
-            maximum = number
-    return maximum
+    return weight/ 0.45
